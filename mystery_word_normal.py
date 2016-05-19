8b51606979cfceb5d06baa24601d7eaa937453b3
hpaasch <hepaasch@gmail.com>
2016-05-19 17:44:20 -0400

    Underlines and letters working.

--- mystery_word_normal.py
+++ mystery_word_normal.py
@@ -1,56 +1,69 @@
+import random
+
+def welcome():
+    print("The computer has picked a secret word. {}: ".format(secret_word))
+    print("_ " * len(secret_word))
+
+def draw_word():
+    for letter in secret_word:
+        if letter in good_guesses:
+            print(letter + ' ', end='')
+        else:
+            print('_ ', end='')
+
+
+get_words = open("/usr/share/dict/words")
+word_list = list(get_words.read().upper().split("\n"))
+
+secret_word = random.choice(word_list)
+
+guesses = 8
+
+secret_word_split = list(secret_word)
+bad_guesses = []
+good_guesses = []
+total_guesses = [] # bonus if i can eliminate need for this
+
+welcome()
+while guesses > 0:
+    draw_word()
+    print("\n")
+    letter_guess = input("You have {} strikes remaining. Pick a letter: ".format(guesses)).upper()
+    if letter_guess in total_guesses:
+        print("Uh, you already guessed that letter.")
+        continue
+
+    if letter_guess in secret_word:
+        total_guesses.append(letter_guess)
+        good_guesses.append(letter_guess)
+        if good_guesses == secret_word_split:
+            print("Wow. You did it. Congrats.")
+            break
+        #replace the underscore with the letter
+        print("Good guess.")
+        print("Incorrect guesses so far: {}.".format(bad_guesses))
+    else:
+        total_guesses.append(letter_guess)
+        bad_guesses.append(letter_guess)
+        guesses -= 1
+        print("And that's a miss.")
+        print("Incorrect guesses so far: {}.".format(bad_guesses))
+else:
+    print("You're out of strikes. You lose. The word was {}.".format(secret_word))
+
+get_words.close()
+
 # DONE import /usr/share/dict/words
 # DONE select a random word from it
 # DONE count the letters and print that as a number and as blanks
 # DONE user input one letter (can assume they only type one letter)
 # DONE correct for case -- .upper user input
 # DONE is the letter in the random word, and print y/n
+# pop letters from secret_word list for each correct guess. then win = list empty
 # update the blanks and print .Maybe split the word?
-# max 8 guesses.
-# tell user how many guesses remain.
-# only decrement guesses for a wrong guess.
-# if same letter guessed again, print that but don't decrement guesses
+# DONE max 8 guesses.
+# DONE tell user how many guesses remain.
+# DONE only decrement guesses for a wrong guess.
+# DONE if same letter guessed again, print that but don't decrement guesses
 # game ends when word guessed or guesses are gone.
-# reveal word if guesses gone.
-
-import random
-
-get_words = open("/usr/share/dict/words")
-word_list = list(get_words.read().upper().split("\n"))
-#print(word_list)
-
-secret_word = random.choice(word_list)
-#print(secret_word)
-
-#print("Welcome to my Hangman game. The computer picks a word and you get 8 misses to solve it.")
-print("Here is the computer's word {}: ".format(secret_word))
-print("_ " * len(secret_word))
-
-guesses = 8
-
-letter_guess = (input("You have {} guesses remaining. Pick a letter: ".format(guesses))).upper()
-#print(letter_guess)
-
-secret_word_split = secret_word.split()
-bad_guesses = []
-good_guesses = []
-
-if letter_guess in bad_guesses or good_guesses:
-    print("Uh, you already guessed that letter. Try another.")
-
-while guesses > 0:
-    if good_guesses == secret_word_split:
-        print("Wow. You did it. Congrats.")
-        break
-    elif letter_guess in secret_word:
-        good_guesses.append(letter_guess)
-        print("Good guess. You still have {} misses remaining.".format(guesses))
-        print("Incorrect guesses so far: {}.".format(bad_guesses))
-        break
-    else:
-        bad_guesses.append(letter_guess)
-        guesses -= 1
-        print("And that's a miss. You have {} strikes remaining.".format(guesses))
-        print("Incorrect guesses so far: {}.".format(bad_guesses))
-        break
-
-get_words.close()
+# DONE reveal word if guesses gone.
