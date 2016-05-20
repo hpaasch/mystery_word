998b3df8b07af2ab39e1bff7d935c08caa71a7a9
hpaasch <hepaasch@gmail.com>
2016-05-20 19:43:09 -0400

    Game level now functional.

--- mystery_word_hard.py
+++ mystery_word_hard.py
@@ -3,14 +3,35 @@ import sys
 
 get_words = open("/usr/share/dict/words")
 word_list = list(get_words.read().upper().split("\n"))
-secret_word = random.choice(word_list)
+word_list_easy = []
+word_list_normal = []
+word_list_hard = []
+for word in word_list:
+    if len(word) == 4:
+        word_list_easy.append(word)
+    if len(word) == 6:
+        word_list_normal.append(word)
+    if len(word) > 9:
+        word_list_hard.append(word)
+
+# secret_word = random.choice(word_list)
+secret_word = ()
 guesses = 8
 
 
-def welcome():
-    print("The computer picked a secret word with {} letters.".format(len(secret_word)))
+def game_start():
+    global secret_word
+    game_level = int(input("Enter 1 for Easy, 2 for Normal or 3 for Hard version of the game. "))
+    if game_level == 1:
+        secret_word = random.choice(word_list_easy)
+    if game_level == 2:
+        secret_word = random.choice(word_list_normal)
+    if game_level == 3:
+        secret_word = random.choice(word_list_hard)
+    return secret_word
+    #print("The computer picked a secret word with {} letters.".format(len(secret_word)))
     # print(secret_word)
-    # BUILD THIS: let user choose difficulty. Easy 4-6 words. Normal 6-10 words. Hard more than 10 words.
+    # Easy 4-6 words. Normal 6-10 words. Hard more than 10 words.
 
 
 
@@ -36,12 +57,14 @@ bad_guesses = []
 good_guesses = []
 total_guesses = []
 
-welcome()
+game_start()
+print("The computer picked a secret word with {} letters.".format(len(secret_word)))
+print(secret_word)
 while guesses > 0:
     print("")
     draw_word()
     print("")
-    # user_guess()
+    # user_guess starts here
     while True:
         letter_guess = input("Remaining strikes: {}. Pick a letter: ".format(guesses)).upper()
         if len(letter_guess) == 0:
@@ -66,6 +89,7 @@ while guesses > 0:
         guesses -= 1
         print("\nAnd that's a miss.")
         print("Incorrect guesses: {}.".format(bad_guesses))
+    # user_guess to here
 else:
     print("You're out of strikes. You lose. The word was {}.".format(secret_word))
 
